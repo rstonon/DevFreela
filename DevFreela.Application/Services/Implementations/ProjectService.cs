ce194136f5596a4c9195381c525ec8a0327d22a0
rstonon <rstonon@gmail.com>
2022-07-27 08:21:43 -0300

    feat: Implementação do Dapper em algumas operações

--- DevFreela.Application/Services/Implementations/ProjectService.cs
+++ DevFreela.Application/Services/Implementations/ProjectService.cs
@@ -1,8 +1,12 @@
-﻿using DevFreela.Application.InputModels;
+﻿using Dapper;
+using DevFreela.Application.InputModels;
 using DevFreela.Application.Services.Interfaces;
 using DevFreela.Application.ViewModels;
 using DevFreela.Core.Entities;
 using DevFreela.Infrastructure.Persistence;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,9 +16,11 @@ namespace DevFreela.Application.Services.Implementations
     public class ProjectService : IProjectService
     {
         private readonly DevFreelaDbContext _dbContext;
-        public ProjectService(DevFreelaDbContext dbContext)
+        private readonly string _connectionString;
+        public ProjectService(DevFreelaDbContext dbContext, IConfiguration configuration)
         {
             _dbContext = dbContext;
+            _connectionString = configuration.GetConnectionString("DevFreelaCs");
         }
 
         public int Create(NewProjectInputModel inputModel)
@@ -66,7 +72,10 @@ namespace DevFreela.Application.Services.Implementations
 
         public ProjectDetailsViewModel GetById(int id)
         {
-            var project = _dbContext.Projects.SingleOrDefault(p => p.Id == id);
+            var project = _dbContext.Projects
+                .Include(p => p.Client)
+                .Include(p => p.Freelancer)
+                .SingleOrDefault(p => p.Id == id);
 
             if (project == null)
             {
@@ -79,7 +88,9 @@ namespace DevFreela.Application.Services.Implementations
                     project.Description,
                     project.TotalCost,
                     project.StartedAt,
-                    project.FinishedAt
+                    project.FinishedAt,
+                    project.Client.FullName,
+                    project.Freelancer.FullName
                 );
 
             return projectsDetailsViewModel;
@@ -91,7 +102,20 @@ namespace DevFreela.Application.Services.Implementations
 
             project.Start();
 
-            _dbContext.SaveChanges();
+            //EF CORE
+            //_dbContext.SaveChanges();
+
+            //=======================================================================================================//
+
+            //Dapper
+            using (var sqlConnection = new SqlConnection(_connectionString))
+            {
+                sqlConnection.Open();
+
+                var script = "UPDATE Projects SET Status = @status, StartedAt = @startedat WHERE Id = @id";
+
+                sqlConnection.Execute(script, new { status = project.Status, startedat = project.StartedAt, id });
+            }
         }
 
         public void Update(UpdateProjectInputModel inputModel)
