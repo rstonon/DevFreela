722ee600a223a77928d3acc396c673044312c7ad
rstonon <rstonon@gmail.com>
2022-09-12 16:30:19 -0300

    feat: Implementação de Testes Unitários

---
+++ DevFreela.UnitTests/Application/Queries/GetAllSkillsCommandHandlerTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DevFreela.UnitTests.Queries
+{
+    public class GetAllSkillsCommandHandlerTests
+    {
+        [Fact]
+        public async Task ThreePrSkillsExist_Executed_ReturnThreeSkillsViewModels()
+        {
+
+        }
+    }
+}
